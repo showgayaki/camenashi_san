19d681f83e5f7eaa5fbc8f32115e30a17d6e9967
showgayaki <knk.lee.kj@gmail.com>
2024-12-03 00:18:59 +0900

    レイアウト揃えるために、結果部分をコードブロックに変更

--- bot/utils/message_builder.py
+++ bot/utils/message_builder.py
@@ -9,8 +9,8 @@ def builder(term: str, records: list[Toilet]) -> str:
     if len(records) == 0:
         return f'{term}はまだおトイレしていません'
 
-    message = f'{term}のおトイレ結果です\n\n'
+    message = f'{term}のおトイレ結果です```'
     for record in records:
         message += f'{record.created_at.strftime('%Y/%m/%d %H:%M:%S')}: {record.category.name}\n'
 
-    return message
+    return f'{message}```'
